5a797dc17eb1aefb5330ccd27baa0a767b63cc05
Wender Firmino <wender.firmino@prof.sc.senac.br>
2025-06-10 21:42:11 +0000

    criação da branch

---
+++ src/main/java/com/senac/pizzademo/dto/ReajustePrecoDTO.java
@@ -0,0 +1,15 @@
+package com.senac.pizzademo.dto;
+
+public class ReajustePrecoDTO {
+    
+    private Double percentual;
+
+    
+    public Double getPercentual() {
+        return percentual;
+    }
+
+    public void setPercentual(Double percentual) {
+        this.percentual = percentual;
+    }
+}
